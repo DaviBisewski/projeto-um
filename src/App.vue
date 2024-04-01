56787ade50d205f1fd4d3908b3b40c160d44b3e8
DaviBisewski <davimbisewski@gmail.com>
2024-04-01 16:49:02 -0300

    forms e submit prontos

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,65 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
-const titulo = ref('Título')
+const titulo = ref('Título');
+
+const mostrarResultado = ref(false)
+
+
+
+const produto = reactive({
+  nome: '',
+  preco: 0,
+  quantidade: 0,
+  categorias: []
+
+})
+
+function formatarPreco(preco) {
+  return `R$ ${preco.toFixed(2).replace('.', ',')}`
+}
 </script>
 
 <template>
-
-  <h1>{{ titulo }}</h1>
   <div class="container">
+    <h1>{{ titulo }}</h1>
     <div class="formulario">
-      <h2>Forms</h2>
-      <input type="text" v-model="titulo"/>
-    </div>
-    <div class="resultado">
+    <form @submit.prevent ="mostrarResultado = !mostrarResultado">
+
+        <h2>Forms</h2>
+        <input type="text" v-model="titulo" />
+        <div class="row">
+          <label for="">Nome:</label>
+          <input type="text" v-model="produto.nome" required>
+        </div>
+        <div class="row">
+          <label for="">Preço:</label>
+          <input type="text" v-model="produto.preco">
+        </div>
+        <div class="row">
+          <label for="">Quantidade:</label>
+          <input type="number" v-model="produto.quantidade">
+        </div>
+        <div class="row">
+          <label for="">Categorias:</label>
+          <input type="text" v-model="produto.categorias">
+        </div>
+        <div class="mostrar">
+          <button @submit ="mostrarResultado = !mostrarResultado">Mostrar</button>
+        </div>
+      </form>
+      </div>
+
+    
+    <div class="resultado" v-if="mostrarResultado">
       <h2>Resultado</h2>
+      <p>Nome: {{ produto.nome }}</p>
+      <p>Preço: {{ formatarPreco(produto.preco *1) }}</p>
+      <p>Quantidade: {{ produto.quantidade }}</p>
+      <p>Categorias: {{ produto.categorias }}</p>
+      <p>{{ mostrarResultado }}</p>
     </div>
+
   </div>
 </template>
 
@@ -22,6 +67,8 @@ const titulo = ref('Título')
 .container {
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
   gap: 1.5rem;
   width: 100vw;
 }
@@ -34,10 +81,32 @@ const titulo = ref('Título')
 }
 
 .formulario {
-  background-color: #c4c4c4;
+  background-color: #a8f0a2;
 }
 
-.resultado{
-  background-color: #c4c500;
+.resultado {
+  background-color: #eeafe6;
+}
+
+.formulario .row {
+  margin: 10px 20px;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+.mostrar {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+}
+
+.mostrar button {
+  font-size: 17px;
+  padding: 5px 10px;
+  border-radius: 10px;
 }
 </style>
