3f303acf1475e0d407377a9865bdb02477718647
DaviBisewski <davimbisewski@gmail.com>
2024-07-05 15:01:09 -0300

    trabalho feito

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const pessoa = reactive({
   cidade: '',
   cep: '',
   linguas: '',
-  hobbies: '',
+  hobbies: [],
 
 })
 
@@ -34,16 +34,16 @@ function formatarPreco(preco) {
     <div class="formulario">
       <form @submit.prevent="mostrarResultado = !mostrarResultado">
         <div class="title">
-        <h1>Formulário</h1>
-      </div>
-        <div class="row">
-           <input type="text" v-model="pessoa.nome" required placeholder="Nome:">
+          <h1>Formulário</h1>
         </div>
         <div class="row">
-            <input type="email" v-model="pessoa.email" placeholder="Email:">
+          <input type="text" v-model="pessoa.nome" required placeholder="Nome:">
         </div>
         <div class="row">
-            <input type="password" v-model="pessoa.senha" placeholder="Senha:">
+          <input type="email" v-model="pessoa.email" placeholder="Email:">
+        </div>
+        <div class="row">
+          <input type="password" v-model="pessoa.senha" placeholder="Senha:">
         </div>
         <div class="row">
           <input type="password" v-model="pessoa.confsenha" placeholder="Confirmar Senha:">
@@ -58,7 +58,7 @@ function formatarPreco(preco) {
           <input type="text" v-model="pessoa.rua" placeholder="Rua">
         </div>
         <div class="row">
-          <input type="text" v-model="pessoa.numero " placeholder="Número">
+          <input type="text" v-model="pessoa.numero" placeholder="Número">
         </div>
         <div class="row">
           <input type="text" v-model="pessoa.bairro" placeholder="Bairro">
@@ -100,95 +100,94 @@ function formatarPreco(preco) {
             <option value="SE">SERGIPE</option>
             <option value="TO">TOCANTINS</option>
           </select>
-        </div>  
+        </div>
 
         <div class="row">
-        <label for="">Linguagem de Programação</label>
+          <label for="">Linguagem de Programação</label>
           <div class="bglh">
-        <div class="checkbox">
-          <input type="checkbox" value="C++" id=""/>
-          <p>C++</p>
+            <div class="checkbox">
+              <input type="radio" value="C++" v-model="pessoa.linguas" id="" />
+              <p>C++</p>
+            </div>
+            <div class="checkbox">
+              <input type="radio" value="Python" v-model="pessoa.linguas" id="" />
+              <p>Python</p>
+            </div>
+            <div class="checkbox">
+              <input type="radio" value="JavaScript" v-model="pessoa.linguas" id="" />
+              <p>JavaScript</p>
+            </div>
+            <div class="checkbox">
+              <input type="radio" value="Java" v-model="pessoa.linguas" id="" />
+              <p>Java</p>
+            </div>
+          </div>
         </div>
-        <div class="checkbox">
-          <input type="checkbox" value="Python" id="" />
-          <p>Python</p>
-        </div>
-        <div class="checkbox">
-          <input type="checkbox" value="JavaScript" id="" />
-          <p>JavaScript</p>
-        </div>
-        <div class="checkbox">
-          <input type="checkbox" value="Java" id="" />
-          <p>Java</p>
-        </div>
-        </div>
-      </div>
         <div class="row">
-        <label for="">Hobbies favoritos</label>
-        <div class="bglh">
-        <div class="checkbox">
-          <input type="checkbox" v-model="musica" id="" />
-          <p>Musica</p>
+          <label for="">Hobbies favoritos</label>
+          <div class="bglh">
+            <div class="checkbox">
+              <input type="checkbox" v-model="pessoa.hobbies" value="Música" id="" />
+              <p>Musica</p>
+            </div>
+            <div class="checkbox">
+              <input type="checkbox" v-model="pessoa.hobbies" value="Esportes" id="" />
+              <p>Esportes</p>
+            </div>
+            <div class="checkbox">
+              <input type="checkbox" v-model="pessoa.hobbies" value="Viagem" id="" />
+              <p>Viagem</p>
+            </div>
+            <div class="checkbox">
+              <input type="checkbox" v-model="pessoa.hobbies" value="Cozinha" id="" />
+              <p>Cozinhar</p>
+            </div>
+          </div>
         </div>
-        <div class="checkbox">
-          <input type="checkbox" v-model="esportes" id="" />
-          <p>Esportes</p>
+        <div class="row">
+          <input type="text" v-model="biografia" id="" placeholder="Biografia:" />
         </div>
-        <div class="checkbox">
-          <input type="checkbox" v-model="viagem" id="" />
-          <p>Viagem</p>
+
+
+        <div class="mostrar">
+          <button @submit="mostrarResultado = !mostrarResultado">Mostrar</button>
         </div>
-        <div class="checkbox">
-          <input type="checkbox" v-model="cozinhar" id="" />
-          <p>Cozinhar</p>
-        </div>
-      </div>
+      </form>
     </div>
-      <div class="row">
-        <input type="text" v-model="biografia" id="" placeholder="Biografia:" />
+
+
+    <div class="resultado" v-if="mostrarResultado">
+      <h2>Resultado</h2>
+      <p>Nome: {{ pessoa.nome }}</p>
+      <p>Email: {{ pessoa.email }}</p>
+      <p>Senha: {{ pessoa.senha }}</p>
+      <p>Confirmar Senha: {{ pessoa.confsenha }}</p>
+      <p>Data de Nascimento: {{ pessoa.data }}</p>
+      <p>Rua: {{ pessoa.rua }}</p>
+      <p>Número: {{ pessoa.numero * 1 }}</p>
+      <p>Bairro: {{ pessoa.bairro }}</p>
+      <p>CEP: {{ pessoa.cep * 1 }}</p>
+      <p>Estado: {{ pessoa.estado }}</p>
+      <p>Hobbies: {{ pessoa.hobbies }}</p>
+      <p>Linguagens: {{ pessoa.linguas }}</p>
     </div>
-
-
-    <div class="mostrar">
-      <button @submit="mostrarResultado = !mostrarResultado">Mostrar</button>
-    </div>
-    </form>
-  </div>
-
-
-  <div class="resultado" v-if="mostrarResultado">
-    <h2>Resultado</h2>
-    <p>Nome: {{ pessoa.nome }}</p>
-    <p>Email: {{ pessoa.email }}</p>
-    <p>Senha: {{ pessoa.senha }}</p>
-    <p>Confirmar Senha: {{ pessoa.confsenha }}</p>
-    <p>Data de Nascimento: {{ pessoa.data }}</p>
-    <p>Rua: {{ pessoa.rua }}</p>
-    <p>Número: {{ pessoa.numero * 1 }}</p>
-    <p>Bairro: {{ pessoa.bairro }}</p>
-    <p>CEP: {{ pessoa.cep * 1 }}</p>
-    <p>Estado: {{ pessoa.estado }}</p>
-    <p>Hobbies: {{ pessoa.hobbies }}</p>
-    <p>Linguagens: {{ pessoa.linguas }}</p>
-  </div>
 
   </div>
 </template>
 
 <style scoped>
-*{
+* {
   padding: 0;
   margin: 0;
   border: 0;
   box-sizing: border-box;
 }
+
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 1.5rem;
-  width: 100vw;
-  height: 100vh;
   background-image: linear-gradient(to left, #1e2124, #282b30, #424549, #424549);
 
 }
@@ -206,12 +205,13 @@ function formatarPreco(preco) {
   align-items: center;
 }
 
-h1{
+h1 {
   color: #fff;
 }
+
 .resultado {
   background-color: #7289da;
-  width: 718px;
+  width: 500px;
   justify-content: center;
   align-items: start;
   animation: spin 2s ease;
@@ -220,21 +220,21 @@ h1{
 }
 
 .formulario .row {
-  margin: 10px 20px;
+  margin: 10px 0px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   text-align: center;
 }
 
-.formulario .row input{
+.formulario .row input {
   padding: 10px;
   border-radius: 0;
   border: 2px solid transparent;
   width: 100%;
 }
 
-.formulario .row input:hover{
+.formulario .row input:hover {
   padding: 10px;
   border-radius: 0;
   border: 2px solid #7289da;
@@ -253,47 +253,49 @@ h1{
 
 }
 
-.checkbox{
+.checkbox {
   display: flex;
   align-items: center;
   justify-content: space-around;
   width: 100px;
 }
 
-.bglh{
+.bglh {
   display: flex;
 }
 
-label{
+label {
   font-size: 1.5rem;
   color: #fff;
 }
 
-select{
+select {
   padding: 10px;
   color: #fff;
 }
 
-.title{
+.title {
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-p{
+
+p {
   color: #fff;
 }
 
-@keyframes spin{
-  to{
+@keyframes spin {
+  to {
     transform: scale(1) translate(0px) rotate(0deg) translateY(0);
   }
-  from{
+
+  from {
     transform: scale(250) translate(2000px) rotate(720deg) translateY(1100px);
   }
 }
 
-.resultado p{
+.resultado p {
   font-size: 16px;
 }
 </style>
